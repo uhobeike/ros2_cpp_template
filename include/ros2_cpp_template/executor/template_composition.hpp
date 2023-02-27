bd41f9e7b144f6c58642b65068ef48464e628e74
uhobeike <beike315@icloud.com>
2023-02-28 02:32:56 +0900

    Feature ros2-cpp-template-executor

---
+++ include/ros2_cpp_template/executor/template_composition.hpp
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2023 Tatsuhiro Ikebe <beike315@icloud.com>
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef ROS2_CPP_TEMPLATE__COMPOSITION__TEMPLATE_COMPOSITION_HPP_
+#define ROS2_CPP_TEMPLATE__COMPOSITION__TEMPLATE_COMPOSITION_HPP_
+
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+namespace Ros2CppTemplate
+{
+
+class TemplateComposition : public rclcpp::Node
+{
+public:
+  explicit TemplateComposition(const rclcpp::NodeOptions & options);
+
+  std::string namespace_;
+  std::string printf_string_;
+  std::chrono::milliseconds printf_ms_;
+
+protected:
+  void initPublisher();
+  void initTimer();
+  void on_timer();
+  void setParam();
+  void getParam();
+
+private:
+  size_t count_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+}  // namespace Ros2CppTemplate
+
+#endif  // ROS2_CPP_TEMPLATE__COMPOSITION__TEMPLATE_COMPOSITION_HPP_
