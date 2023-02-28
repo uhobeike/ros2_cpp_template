67eb78541b1603962c2e3e1e6c6d6a90038a966d
uhobeike <beike315@icloud.com>
2023-02-28 12:33:28 +0900

    Fix launch-yaml-load

--- launch/template_executor.launch.py
+++ launch/template_executor.launch.py
@@ -6,41 +6,24 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.actions import GroupAction
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
     ros2_cpp_template_dir = get_package_share_directory('ros2_cpp_template')
-    config_dir = os.path.join(ros2_cpp_template_dir, 'config')
-
-    params_file = LaunchConfiguration('params_file')
-
-    declare_params_file_cmd = DeclareLaunchArgument(
-        'params_file',
-        default_value=os.path.join(
-            config_dir, 'template.param.yaml'),
-        description='Param')
-
-    configured_params = RewrittenYaml(
-        source_file=params_file,
-        root_key="",
-        param_rewrites={},
-        convert_types=True)
+    params_file = os.path.join(ros2_cpp_template_dir, 'config', 'template.param.yaml')
 
     launch_node = GroupAction([
         Node(
             name='ros2_cpp_template_executor',
             package='ros2_cpp_template',
             executable='template_executor',
-            parameters=[configured_params],
+            parameters=[params_file],
             output='screen')
     ])
 
     ld = LaunchDescription()
-    ld.add_action(declare_params_file_cmd)
     ld.add_action(launch_node)
 
     return ld
